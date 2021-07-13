de5e0a649ce3272b44fa7016d7a7d115ce8db569
Mariano <marianoperri@gmail.com>
2021-07-13 13:15:48 -0300

    arreglando vista mapa, cargar posición

--- View/cargarPosicion.php
+++ View/cargarPosicion.php
@@ -1,4 +1,4 @@
-{{> header}}
+{{> headerMap}}
 
 <main class="main-home" >
     <section class="tareas">
@@ -11,18 +11,22 @@
     <section class="reporte" >
         <form class="formulario-posicion" action="/cargarDatosViaje/cargarPosicion"}; method="POST">
             <div>
-                <label id="numeroViaje">Numero De Viaje:</label>
-                <input class="campos" type="number" name="numeroViaje" id="numeroViaje" REQUIRED>
-                <div id="mapa" class="mapa" style=""></div>
-                <!--            cuando avancemos hay que ocultar las coordenadas ahora se dejan para ver que las captura real desde el navegador web, falta mobile-->
+            <label id="numeroViaje">Numero De Viaje:</label>
+            <!--            <input class="campos" type="number" name="numeroViaje" id="numeroViaje" REQUIRED>-->
+            <div id="mapa" class="mapa" style=""></div>
+            <!--            cuando avancemos hay que ocultar las coordenadas ahora se dejan para ver que las captura real desde el navegador web, falta mobile-->
+            <labe>Posición actual</labe>
+            <input class="campos" type="text" name="coordenadas" id="coordenadas" REQUIRED>
             </div>
             <div class="col-posicion-input">
-                <labe>Coordenadas</labe>
-                <input class="campos" type="text" name="coordenadas" id="coordenadas" REQUIRED>
                 <label id="fecha">Fecha:</label>
                 <input class="campos" type="date" name="fecha" id="fecha" required >
                 <label id="hora">Hora:</label>
                 <input class="campos" type="time" name="hora" id="hora" required >
+                <label id="km">Kilometros:</label>
+                <input class="campos" type="numero" name="km" id="km" required >
+                <label id="gasto">Descripcion:</label>
+                <input class="campos" type="text" name="gasto" id="gasto" required >
                 <button type="submit" class="boton">Agregar</button>
             </div>
         </form>
