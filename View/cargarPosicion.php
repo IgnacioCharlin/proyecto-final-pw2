be459ff3648eb1553164e9543bc5b11341d556b4
Charlin Ignacio <ignaciocharlin1995@gmail.com>
2021-07-14 13:27:49 -0300

    pull

--- View/cargarPosicion.php
+++ View/cargarPosicion.php
@@ -9,24 +9,29 @@
         </ul>
     </section>
     <section class="reporte" >
-        <form class="formulario-posicion" action="/cargarDatosViaje/cargarPosicion"}; method="POST">
+        <form class="formulario-posicion" action="/cargarDatosViaje/cargarPosicion" method="POST">
             <div>
-            <label id="numeroViaje">Numero De Viaje:</label>
-            <!--            <input class="campos" type="number" name="numeroViaje" id="numeroViaje" REQUIRED>-->
-            <div id="mapa" class="mapa" style=""></div>
-            <!--            cuando avancemos hay que ocultar las coordenadas ahora se dejan para ver que las captura real desde el navegador web, falta mobile-->
-            <labe>Posición actual</labe>
-            <input class="campos" type="text" name="coordenadas" id="coordenadas" REQUIRED>
+                <div id="mapa" class="mapa" style=""></div>
+                <!--            cuando avancemos hay que ocultar las coordenadas ahora se dejan para ver que las captura real desde el navegador web, falta mobile-->
+                <labe>Posición actual</labe>
+                <input class="campos" type="text" name="coordenadas" id="coordenadas" REQUIRED>
             </div>
             <div class="col-posicion-input">
+                <label id="numeroViaje">Numero De Viaje:</label>
+                <!--            <input class="campos" type="number" name="numeroViaje" id="numeroViaje" REQUIRED>-->
                 <label id="fecha">Fecha:</label>
                 <input class="campos" type="date" name="fecha" id="fecha" required >
                 <label id="hora">Hora:</label>
                 <input class="campos" type="time" name="hora" id="hora" required >
                 <label id="km">Kilometros:</label>
                 <input class="campos" type="numero" name="km" id="km" required >
+
+            </div>
+            <div class="col-posicion-input">
                 <label id="gasto">Descripcion:</label>
                 <input class="campos" type="text" name="gasto" id="gasto" required >
+                <label id="gasto">Gasto:</label>
+                <input class="campos" type="number" name="gasto" id="gasto" REQUIRED>
                 <button type="submit" class="boton">Agregar</button>
             </div>
         </form>
